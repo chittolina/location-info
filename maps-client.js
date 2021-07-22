a2d9e6c9600bbc44f36cd477e846a9d8cb9b14b0
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2021-07-22 17:15:22 -0300

    dont hardcode API url

--- maps-client.js
+++ maps-client.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const client = axios.create({
   baseURL: "https://maps.googleapis.com/maps/api",
   params: {
-    key: "AIzaSyBNW9ny7Q9TS1iRLYWgrWo4CwAb3wmrEik",
+    key: process.env.GOOGLE_API_KEY,
   },
 });
 
